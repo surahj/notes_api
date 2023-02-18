10e2e29ba9a7be7a0b26520f1456959a0b3b3f55
surahj <odebodeopeyemi@gmail.com>
2023-02-18 02:31:52 -0800

    Fix error

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,7 @@
 module.exports = {
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
   }
 };
